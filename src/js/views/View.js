933fd0a59b13ca5b60dd9e82c94ed46d7353208a
saira <saira@ceribs.com>
2021-10-09 11:53:22 +0530

    trying t0  debug

--- src/js/views/View.js
+++ src/js/views/View.js
@@ -8,6 +8,7 @@ export default class View {
       return this.renderError();
 
     this._data = data;
+    console.log(data);
     const markup = this._generateMarkup();
 
     if (!render) return markup;
